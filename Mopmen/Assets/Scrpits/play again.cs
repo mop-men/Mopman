c6db86677678d208df59cf1ce49655c18d78fefb
TThisRubberDuck <47885326+TThisRubberDuck@users.noreply.github.com>
2019-05-24 12:29:55 +0100

    edited colouring and scenes
    changed the colour of the text boxes so they are readable on the main stage and if they cover the dark parts, connected teh win and lose scenes, buttons are still in construction

---
+++ Mopmen/Assets/Scrpits/play again.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playagain : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+// Detects if any key has been pressed down.
+    void Update()
+    {
+        {
+        SceneManager.LoadScene("Main");
+        }
+    }
+}
